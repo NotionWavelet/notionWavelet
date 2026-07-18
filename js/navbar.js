c21abdc4b088a1e6b5df23cad8f23217cb7b37d9
NotionWavelet <notionwavelet@gmail.com>
2026-07-18 17:57:17 +0200

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Contenido de la barra de navegación
+    // Contenido de la barra de navegación adaptado para el taller
     var navbarContent = `
     <div class="header_inner header_inner-expand-sm header_inner-custom d-flex flex-row align-items-center justify-content-start">
 			<nav class="main_nav">
 				<ul>
-					<li><img src="images/logo.png" alt="Notion Wavelet Logo" width="30"><a href="index.html">Notion Wavelet</a></li>
+					<li><img src="images/logo.png" alt="Notion Wavelet Logo" width="30"><a href="index.html">Noción de Ondícula</a></li>
+					<li><a href="#demo-section">Video Demo</a></li>
 					<li><a href="contact.html">Contacto</a></li>
 				</ul>
 			</nav>
-
 		</div>
     `;
-
 
     // Selecciona el elemento donde quieres incluir la barra de navegación
     var navbarContainer = document.querySelector("#navbar_container");
@@ -21,6 +20,4 @@ document.addEventListener("DOMContentLoaded", function() {
     if (navbarContainer) {
         navbarContainer.innerHTML = navbarContent;
     }
-
-
 });
